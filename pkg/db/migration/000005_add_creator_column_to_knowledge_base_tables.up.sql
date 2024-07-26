577a14a33b403e8087bed3872dc922c7b41044ee
gary.y <s14031403@gmail.com>
2024-07-26 15:49:22 +0800

    fix(kb): fix db migration error

--- pkg/db/migration/000005_add_creator_column_to_knowledge_base_tables.up.sql
+++ pkg/db/migration/000005_add_creator_column_to_knowledge_base_tables.up.sql
@@ -1,9 +1,6 @@
 BEGIN;
 -- Add the new column to the knowledge_base table
-ALTER TABLE knowledge_base
-ADD COLUMN creator_uid UUID NOT NULL;
-
+ALTER TABLE knowledge_base ADD COLUMN creator_uid UUID;
 -- Add comment for the new column
 COMMENT ON COLUMN knowledge_base.creator_uid IS 'UUID of the creator of the knowledge base';
-
 COMMIT;
