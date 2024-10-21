1e45ac8553b220e5afb8235c37867f1a1caee569
gary.y <s14031403@gmail.com>
2024-10-21 18:09:15 +0800

    feat(blob): provide the upload object url endpoint

--- go.mod
+++ go.mod
@@ -13,7 +13,7 @@ require (
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.1
 	github.com/influxdata/influxdb-client-go/v2 v2.12.3
-	github.com/instill-ai/protogen-go v0.3.3-alpha.0.20241018045010-dcc80f850d9d
+	github.com/instill-ai/protogen-go v0.3.3-alpha.0.20241021084925-9a2ae77e0adb
 	github.com/instill-ai/usage-client v0.3.0-alpha.0.20240319060111-4a3a39f2fd61
 	github.com/instill-ai/x v0.3.0-alpha.0.20231219052200-6230a89e386c
 	github.com/knadh/koanf v1.5.0
@@ -105,4 +105,4 @@ require (
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
-// replace github.com/instill-ai/protogen-go v0.3.3-alpha.0.20241001150423-8d8b9e2fa860 => ./protogen-go
+// replace github.com/instill-ai/protogen-go => ./protogen-go
---
+++ pkg/handler/object.go
@@ -0,0 +1,60 @@
+package handler
+
+import (
+	"context"
+	"fmt"
+
+	"go.uber.org/zap"
+
+	"github.com/gofrs/uuid"
+	"github.com/instill-ai/artifact-backend/pkg/customerror"
+	"github.com/instill-ai/artifact-backend/pkg/logger"
+	artifactpb "github.com/instill-ai/protogen-go/artifact/artifact/v1alpha"
+)
+
+// GetObjectUploadURL returns the upload URL for an object.
+func (ph *PublicHandler) GetObjectUploadURL(ctx context.Context, req *artifactpb.GetObjectUploadURLRequest) (*artifactpb.GetObjectUploadURLResponse, error) {
+	log, _ := logger.GetZapLogger(ctx)
+	authUID, err := getUserUIDFromContext(ctx)
+	if err != nil {
+		err := fmt.Errorf("failed to get user id from header: %v. err: %w", err, customerror.ErrUnauthenticated)
+		return nil, err
+	}
+	creatorUID, err := uuid.FromString(authUID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse creator uid. err: %w", err)
+	}
+
+	ns, err := ph.service.GetNamespaceByNsID(ctx, req.GetNamespaceId())
+	if err != nil {
+		log.Error(
+			"failed to get namespace ",
+			zap.Error(err),
+			zap.String("owner_id(ns_id)", req.GetNamespaceId()),
+			zap.String("auth_uid", authUID))
+		return nil, fmt.Errorf("failed to get namespace. err: %w", err)
+	}
+	// ACL - check user's permission to upload object in the namespace
+	err = ph.service.CheckNamespacePermission(ctx, ns)
+	if err != nil {
+		log.Error(
+			"failed to check namespace permission",
+			zap.Error(err),
+			zap.String("owner_id(ns_id)", req.GetNamespaceId()),
+			zap.String("auth_uid", authUID))
+		return nil, fmt.Errorf("failed to check namespace permission. err: %w", err)
+	}
+
+	// Call the service to get the upload URL
+	response, err := ph.service.GetUploadURL(ctx, req, ns.NsUID, creatorUID)
+	if err != nil {
+		log.Error("failed to get upload URL", zap.Error(err))
+		return nil, fmt.Errorf("failed to get upload URL. err: %w", err)
+	}
+
+	return response, nil
+}
+
+func (ph *PublicHandler) GetObjectDownloadURL(ctx context.Context, req *artifactpb.GetObjectDownloadURLRequest) (*artifactpb.GetObjectDownloadURLResponse, error) {
+	return nil, nil
+}
