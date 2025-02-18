915b66ebac455e3f01bd2a209a4e851ebf2d33c2
HR Wu <5631010+heiruwu@users.noreply.github.com>
2025-02-18 18:34:58 +0800

    chore(migration): fix migration script (#148)
    Because
    
    - fix migration script
    
    This commit
    
    - fix migration script

--- pkg/db/migration/000021_rename_content_and_add_chunk_type.up.sql
+++ pkg/db/migration/000021_rename_content_and_add_chunk_type.up.sql
@@ -2,8 +2,8 @@ BEGIN;
 
 -- Add new columns file_type and content_type for text_chunk
 ALTER TABLE text_chunk
-ADD COLUMN file_type VARCHAR(255) NOT NULL,
-ADD COLUMN content_type VARCHAR(255) NOT NULL;
+ADD COLUMN file_type VARCHAR(255),
+ADD COLUMN content_type VARCHAR(255);
 
 -- Add a comment for the new column
 COMMENT ON COLUMN text_chunk.file_type IS 'file type';
@@ -11,8 +11,8 @@ COMMENT ON COLUMN text_chunk.content_type IS 'content type';
 
 -- Add new columns file_type and content_type for text_chunk
 ALTER TABLE embedding
-ADD COLUMN file_type VARCHAR(255) NOT NULL,
-ADD COLUMN content_type VARCHAR(255) NOT NULL;
+ADD COLUMN file_type VARCHAR(255),
+ADD COLUMN content_type VARCHAR(255);
 
 -- Add a comment for the new column
 COMMENT ON COLUMN embedding.file_type IS 'file type';
