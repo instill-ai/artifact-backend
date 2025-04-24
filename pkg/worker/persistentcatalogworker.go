f3d410967ca577e8fbf3bfa49669ab99b6ea2954
Chang, Hui-Tang <huitang.chang@instill.tech>
2025-04-24 18:18:41 +0800

    fix(artifact): use VLM pipeline to convert ppt/pptx files

--- pkg/worker/persistentcatalogworker.go
+++ pkg/worker/persistentcatalogworker.go
@@ -382,9 +382,7 @@ func (wp *persistentCatalogFileToEmbWorkerPool) processConvertingFile(ctx contex
 	switch file.Type {
 	case artifactpb.FileType_FILE_TYPE_PDF.String(),
 		artifactpb.FileType_FILE_TYPE_DOC.String(),
-		artifactpb.FileType_FILE_TYPE_DOCX.String(),
-		artifactpb.FileType_FILE_TYPE_PPT.String(),
-		artifactpb.FileType_FILE_TYPE_PPTX.String():
+		artifactpb.FileType_FILE_TYPE_DOCX.String():
 		convertedMD, err = wp.svc.ConvertToMDModel(ctx, file.UID, file.CreatorUID, requesterUID, base64Data, artifactpb.FileType(artifactpb.FileType_value[file.Type]))
 		if err != nil {
 			logger.Error("Failed to convert pdf to md using docling model, fallback to pipeline.")
