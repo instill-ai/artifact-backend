99a9d294450e15f4bd1c343f3e5b600d83c8bbb8
george <george.strong@instill.tech>
2025-05-21 23:29:17 +0100

    "fix: reverts previous commit"
    This reverts commit 6af8d63302c266248b362c4ef4acd9b8bc0d0712.

--- pkg/client/grpc/model.go
+++ pkg/client/grpc/model.go
@@ -3,10 +3,8 @@ package grpcclient
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/backoff"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 
@@ -16,30 +14,6 @@ import (
 
 	pb "github.com/instill-ai/protogen-go/model/model/v1alpha"
 )
-
-// ModelClientConfig contains configuration for model client
-type ModelClientConfig struct {
-	MaxRetries           int
-	InitBackoff          time.Duration
-	MaxBackoff           time.Duration
-	BackoffMultiplier    float64
-	RetryableStatusCodes []string
-}
-
-// DefaultModelClientConfig provides default retry configuration
-func DefaultModelClientConfig() *ModelClientConfig {
-	return &ModelClientConfig{
-		MaxRetries:        5,
-		InitBackoff:       5 * time.Second,
-		MaxBackoff:        15 * time.Second,
-		BackoffMultiplier: 1.5,
-		RetryableStatusCodes: []string{
-			"UNAVAILABLE",
-			"RESOURCE_EXHAUSTED",
-			"INTERNAL",
-		},
-	}
-}
 
 // NewModelPublicClient returns an initialized gRPC client for the Model public
 // API.
@@ -57,39 +31,7 @@ func NewModelPublicClient(ctx context.Context) (pb.ModelPublicServiceClient, *gr
 		clientDialOpts = grpc.WithTransportCredentials(insecure.NewCredentials())
 	}
 
-	// Configure retry policy
-	retryConfig := DefaultModelClientConfig()
-
-	// Configure connection backoff parameters
-	connParams := grpc.ConnectParams{
-		Backoff: backoff.Config{
-			BaseDelay:  retryConfig.InitBackoff,
-			Multiplier: retryConfig.BackoffMultiplier,
-			Jitter:     0.2,
-			MaxDelay:   retryConfig.MaxBackoff,
-		},
-		MinConnectTimeout: 5 * time.Second,
-	}
-
-	clientConn, err := grpc.NewClient(
-		fmt.Sprintf("%v:%v", config.Config.ModelBackend.Host, config.Config.ModelBackend.PublicPort),
-		grpc.WithConnectParams(connParams),
-		clientDialOpts,
-		grpc.WithDefaultServiceConfig(`{
-			"methodConfig": [{
-				"name": [{"service": "model.model.v1alpha.ModelPublicService"}],
-				"waitForReady": true,
-				"retryPolicy": {
-					"MaxAttempts": 4,
-					"InitialBackoff": "5s",
-					"MaxBackoff": "15s",
-					"BackoffMultiplier": 1.5,
-					"RetryableStatusCodes": ["UNAVAILABLE", "RESOURCE_EXHAUSTED", "INTERNAL"]
-				}
-			}]
-		}`),
-		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(constant.MaxPayloadSize), grpc.MaxCallSendMsgSize(constant.MaxPayloadSize)),
-	)
+	clientConn, err := grpc.NewClient(fmt.Sprintf("%v:%v", config.Config.ModelBackend.Host, config.Config.ModelBackend.PublicPort), clientDialOpts, grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(constant.MaxPayloadSize), grpc.MaxCallSendMsgSize(constant.MaxPayloadSize)))
 	if err != nil {
 		logger.Error(err.Error())
 		return nil, nil
